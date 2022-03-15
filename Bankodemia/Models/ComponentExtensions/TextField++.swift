85a25c613526eb86ade1b87ebdfbf7dc301ef5c2
NeryGuevara <96453929+NeryGuevara@users.noreply.github.com>
2022-03-14 19:52:36 -0600

    Extensiones de UILabel y UIButton

--- Bankodemia/Models/ComponentExtensions/TextField++.swift
+++ Bankodemia/Models/ComponentExtensions/TextField++.swift
@@ -14,7 +14,7 @@ extension UITextField{
             self.textAlignment = .center
             self.backgroundColor = .clear
             self.layer.cornerRadius = 4
-            self.layer.borderColor = CGColor(red: 0, green: 204/255, blue: 204/255, alpha: 0.9)
+            self.layer.borderColor = ConstantsCGColor.greenBlue
             self.layer.borderWidth = 1
         }else {
             
