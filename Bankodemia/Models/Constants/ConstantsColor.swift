aafbdf4dddb2f7727cc1a72c1f86c1424924c5dc
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-03-16 17:00:13 -0600

    add struct to text field and uiview

--- Bankodemia/Models/Constants/ConstantsColor.swift
+++ Bankodemia/Models/Constants/ConstantsColor.swift
@@ -10,8 +10,13 @@ import UIKit
 
 struct ConstantsCGColor{
     static let greenBlue : CGColor = CGColor(red: 0, green: 204/255, blue: 204/255, alpha: 0.9)
+    
+    static let kodemiaGrey : CGColor = CGColor(red: 127/255, green: 127/255, blue: 127/255, alpha: 0.9)
 }
 
 struct ConstantsUIColor{
     static let greenBlue : UIColor = UIColor(red: 0, green: 204/255, blue: 204/255, alpha: 0.9)
+    
+    static let kodemiaGrey : UIColor = UIColor(red: 127/255, green: 127/255, blue: 127/255, alpha: 0.9)
+
 }
--- Bankodemia/Modules/Login/LoginViewController.swift
+++ Bankodemia/Modules/Login/LoginViewController.swift
@@ -11,6 +11,7 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
