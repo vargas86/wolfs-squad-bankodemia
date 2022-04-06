a9598e41986c39d5e0547d70ea4f55a60b4f0017
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-04-06 09:18:43 -0500

    fix successful Screen of add contact

--- Bankodemia/Delegates/SceneDelegate.swift
+++ Bankodemia/Delegates/SceneDelegate.swift
@@ -17,7 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         //guard let _ = (scene as? UIWindowScene) else { return }
-        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = scene
+        let loginViewController = LoginViewController()
+        window?.rootViewController = loginViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
